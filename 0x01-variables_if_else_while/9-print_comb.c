56f682ccfd9d8a583dccc017907753f3db57da60
Favour Okechukwu <favourapps17@gmail.com>
2022-03-12 04:19:08 +0100

    fixed errors in checker

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Print single digit numbers in the formart
+ * main - Entry point
  *
- * Description: Sepaeated by commas
- * Return: Always(0) Success
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar(i + 48);
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	if (i != 57)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-
+	putchar(10);
 	return (0);
 }
