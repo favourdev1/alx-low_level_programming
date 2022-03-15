8e341807134a384de7cb19340ca3f62e63dc448c
favourpro <favourapps17@gmail.com>
2022-03-15 12:42:15 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - writes the character _putchar to stdout
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	//return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef FILE_Header 
+#define FILE_Header
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+void fibonacci(void);
+
+int _putchar(char c);
+#endif
