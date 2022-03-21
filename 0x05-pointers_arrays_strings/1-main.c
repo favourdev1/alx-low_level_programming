5a922da079f0eeb36b439ec5189b67aa167d08b3
favourpro <favourapps17@gmail.com>
2022-03-21 14:59:56 -0700

    second commit

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -1,17 +1,20 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#include "main.h"
+#include <stdio.h>
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-int _putchar(char);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
 
-#endif
+	a = 98;
+	b = 42;
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
